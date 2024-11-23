24e46532cfb9c93b30faa5511bbd0283a68d1cd3
Ademicope <84354061+Ademicope@users.noreply.github.com>
2024-11-24 00:40:04 +0100

    Button controls done

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -15,14 +15,15 @@ public class CarController : MonoBehaviour
     [SerializeField] Transform frontLeftTransform;
     [SerializeField] Transform backLeftTransform;
 
-    private float verticalInput, horizontalInput;
+    private float verticalInput = 0;
+    private float horizontalInput = 0;
 
-    public float maxMotorTorque = 300f;
+    public float maxMotorTorque = 2000f;
     //public float acceleration = 300f;
-    public float acceleration = 0f;
+    public float acceleration = 500f;
     public float brakingForce = 150f;
     public float maxTurnAngle = 30f;
-    public float accelerationRate = 0f;
+    //public float accelerationRate = 0f;
     public float maxSpeed = 220f;
 
     private float currentAcceleration = 0;
@@ -31,6 +32,8 @@ public class CarController : MonoBehaviour
 
     public float currentSpeed = 0;
     private float motorTorque = 0f;
+
+    private bool isUsingUIBotton = false;
 
     private Rigidbody carRb;
 
@@ -43,19 +46,23 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
+        if (!isUsingUIBotton)
+        {
+            verticalInput = Input.GetAxis("Vertical");
+            horizontalInput = Input.GetAxis("Horizontal");
+        }
     }
 
     public void GetSpeed()
     {
         Vector3 localVelocity = transform.InverseTransformDirection(carRb.velocity);
         float forwardSpeed = localVelocity.z;
-        currentSpeed = Mathf.Max(forwardSpeed * 3.6f); // Convert m/s to km/h
-        Debug.Log("Current Speed: " + currentSpeed + " km/h");
+        currentSpeed = forwardSpeed * 3.6f; // Convert m/s to km/h
+        //Debug.Log("Current Speed: " + currentSpeed + " km/h");
 
         if (verticalInput > 0 && currentSpeed < maxSpeed)
         {
+            Debug.Log($"Vertical Input: {verticalInput}");
             //currentSpeed += accelerationRate * Time.deltaTime;
 
             // Calculate the motor torque based on current speed
@@ -63,14 +70,13 @@ public class CarController : MonoBehaviour
             // Apply motor torque to the rear wheels
             backRight.motorTorque = motorTorque;
             backLeft.motorTorque = motorTorque;
+            
         }
-        else// if (verticalInput == 0 || currentSpeed > maxSpeed)
+        else
         {
             motorTorque = 0;
             backRight.motorTorque = motorTorque;
             backLeft.motorTorque = motorTorque;
-            //currentSpeed = Mathf.Max(currentSpeed - (accelerationRate * 0.5f * Time.deltaTime), 0);
-
         }
 
         // Simulate rolling friction by reducing speed when no input is given
@@ -81,9 +87,8 @@ public class CarController : MonoBehaviour
     {
         if (carRb.velocity.magnitude > 0)
         {
-            float rollingResistance = 10f; // Arbitrary value for resistance
+            float rollingResistance = 0.1f; // Arbitrary value for resistance
             Vector3 resistanceForce = -carRb.velocity.normalized * rollingResistance;
-            //currentSpeed = Mathf.Max(0f, currentSpeed - rollingResistance * Time.deltaTime);
             carRb.AddForce(resistanceForce, ForceMode.Acceleration);
         }
     }
@@ -91,10 +96,33 @@ public class CarController : MonoBehaviour
     void FixedUpdate()
     {
         GetSpeed();
-        // Get acceleration and decceleration from vertical input
-        currentAcceleration = acceleration * verticalInput;
 
-        if (Input.GetKey(KeyCode.Space))
+        if (verticalInput > 0)
+        {
+            // Get acceleration and decceleration from vertical input
+            currentAcceleration = acceleration * verticalInput;
+
+
+
+            //Apply acceleration to front wheels
+            frontRight.motorTorque = currentAcceleration;
+            frontLeft.motorTorque = currentAcceleration;
+        }
+        //Apply brake to all wheels
+        ApplyBrakes(currentBrakeForce);
+
+        // Handle steering
+        HandleSteering();
+
+        //ApplyRollingFriction();
+
+        //update wheel meshes
+        UpdateWheelMeshes();
+    }
+
+    private void ApplyBrakes(float brakeForce)
+    {
+        if (Input.GetKey(KeyCode.Space) || verticalInput < 0)
         {
             currentBrakeForce = brakingForce;
         }
@@ -103,29 +131,10 @@ public class CarController : MonoBehaviour
             currentBrakeForce = 0;
         }
 
-        //Apply acceleration to front wheels
-        frontRight.motorTorque = currentAcceleration;
-        frontLeft.motorTorque = currentAcceleration;
-
-        //Apply brake to all wheels
-        ApplyBrakes(brakingForce);
-
-        // Handle steering
-        HandleSteering();
-
-        //update wheel meshes
-        UpdateWheel(frontRight, frontRightTransform);
-        UpdateWheel(frontLeft, frontLeftTransform);
-        UpdateWheel(backRight, backRightTransform);
-        UpdateWheel(backLeft, backLeftTransform);
-    }
-
-    private void ApplyBrakes(float brakeForce)
-    {
-        frontRight.brakeTorque = brakeForce;
-        frontLeft.brakeTorque = brakeForce;
-        backRight.brakeTorque = brakeForce;
-        backLeft.brakeTorque = brakeForce;
+        frontRight.brakeTorque = currentBrakeForce;
+        frontLeft.brakeTorque = currentBrakeForce;
+        backRight.brakeTorque = currentBrakeForce;
+        backLeft.brakeTorque = currentBrakeForce;
     }
 
     private void HandleSteering()
@@ -148,21 +157,54 @@ public class CarController : MonoBehaviour
         trans.rotation = rotation;
     }
 
-    //Button controls
-    public void Accelerate()
+    private void UpdateWheelMeshes()
     {
+        UpdateWheel(frontRight, frontRightTransform);
+        UpdateWheel(frontLeft, frontLeftTransform);
+        UpdateWheel(backRight, backRightTransform);
+        UpdateWheel(backLeft, backLeftTransform);
+    }
+    //Button controls
+    public void AccelerateButtonDown()
+    {
+        isUsingUIBotton = true;
         Debug.Log("Accelerating");
         verticalInput = 1f;
-        accelerationRate = 5f;
-        acceleration = 300f;
     }
 
-    public void StopAccelerate()
+    public void AccelerateButtonup()
     {
-        Debug.Log("Accelerating");
+        Debug.Log("Stopping");
         verticalInput = 0f;
-        accelerationRate = 0f;
-        acceleration = 0f;
+    }
+    
+    public void BrakeButtonDown()
+    {
+        verticalInput = -1f;
     }
 
+    public void BrakeButtonUp()
+    {
+        verticalInput = 0f;
+    }
+
+    public void RightButtonDown()
+    {
+        horizontalInput = 1f;
+    }
+
+    public void LeftButtonDown()
+    {
+        horizontalInput = -1f;
+    }
+
+    public void RightButtonUp()
+    {
+        horizontalInput = 0f;
+    }
+
+    public void LeftButtonUp()
+    {
+        horizontalInput = 0f;
+    }
 }
