774a67609f8b23b6b886ad744d095c0fe8f6d35e
Ademicope <84354061+Ademicope@users.noreply.github.com>
2024-12-06 03:30:06 +0100

    implementing mini map direction

--- Assets/Scripts/StartAndEndPoint.cs
+++ Assets/Scripts/StartAndEndPoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartAndEndPoint : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class StartAndEndPoint : MonoBehaviour
     public bool isReachDestination = false;
 
     public GameManager gameManager;
+
+    public GameObject carPointer;
+    public GameObject destinationPointer;
+    //public RectTransform miniMapRect;
 
     GameObject destinationPoint;
 
@@ -22,7 +27,7 @@ public class StartAndEndPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdatePointerPositionsAndDirection();
     }
 
     private void SetDestination()
@@ -50,6 +55,46 @@ public class StartAndEndPoint : MonoBehaviour
             isReachDestination = true;
             Debug.Log("destination reached");
             gameManager.GameWon();
+        }
+    }
+
+    /*private void UpdatePointerPositions()
+    {
+        Vector2 carMiniMapPos = WorldToMiniMap(transform.position);
+        Vector2 destMiniMapPos = WorldToMiniMap(destinationPoint.transform.position);
+
+        carPointer.rectTransform.anchoredPosition = carMiniMapPos;
+        destinationPointer.rectTransform.anchoredPosition = destMiniMapPos;
+
+        // Rotate the destination pointer to point toward the destination
+        Vector2 direction = destMiniMapPos - carMiniMapPos;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        destinationPointer.rectTransform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private Vector2 WorldToMiniMap(Vector3 worldPosition)
+    {
+        Vector3 localPosition = worldPosition - transform.position; // Relative to car
+        float miniMapScale = miniMapRect.sizeDelta.x / 100f; // Adjust the scale
+        return new Vector2(localPosition.x, localPosition.z) * miniMapScale;
+    }*/
+
+    private void UpdatePointerPositionsAndDirection()
+    {
+        // Position the car pointer slightly above the car for visibility
+        carPointer.transform.position = transform.position + Vector3.up * 5f;
+
+        // Position the destination pointer slightly above the destination point
+        destinationPointer.transform.position = destinationPoint.transform.position + Vector3.up * 5f;
+
+        // Calculate direction from the car to the destination
+        Vector3 directionToDestination = destinationPoint.transform.position - transform.position;
+
+        // Rotate the car pointer to point towards the destination
+        if (directionToDestination != Vector3.zero) // Avoid errors if the direction is zero
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(directionToDestination);
+            carPointer.transform.rotation = Quaternion.Lerp(carPointer.transform.rotation, targetRotation, Time.deltaTime * 5f);
         }
     }
 }
